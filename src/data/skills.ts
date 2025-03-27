03c636485aa92c8aa5d6d982ebce42018896d07a
Croquignoles <htavard@ensc.fr>
2025-03-28 00:04:00 +0100

    path fixing deploy

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,21 +1,21 @@
 import { ESkillCategory } from "@/types/enums"
 
 export const skillList: SkillTag[] = [
-  {name: 'accessibility', icon: 'src/assets/pictures/icons/accessibility.png', category: ESkillCategory.TOOL},
-  {name: 'csharp', icon: 'src/assets/pictures/icons/csharp.png', category: ESkillCategory.TECHNOLOGY},
-  {name: 'css', icon: 'src/assets/pictures/icons/css.png', category: ESkillCategory.DESIGN},
-  {name: 'figma', icon: 'src/assets/pictures/icons/figma.png', category: ESkillCategory.DESIGN},
-  {name: 'git', icon: 'src/assets/pictures/icons/git.png', category: ESkillCategory.TECHNOLOGY},
-  {name: 'html', icon: 'src/assets/pictures/icons/html.png', category: ESkillCategory.TECHNOLOGY},
-  {name: 'javascript', icon: 'src/assets/pictures/icons/javascript.png', category: ESkillCategory.TECHNOLOGY},
-  {name: 'jira', icon: 'src/assets/pictures/icons/jira.png', category: ESkillCategory.TOOL},
-  {name: 'netcore', icon: 'src/assets/pictures/icons/netcore.png', category: ESkillCategory.TECHNOLOGY},
-  {name: 'nuxt', icon: 'src/assets/pictures/icons/nuxt.png', category: ESkillCategory.TECHNOLOGY},
-  {name: 'php', icon: 'src/assets/pictures/icons/php.png', category: ESkillCategory.TECHNOLOGY},
-  {name: 'python', icon: 'src/assets/pictures/icons/python.png', category: ESkillCategory.TECHNOLOGY},
-  {name: 'sass', icon: 'src/assets/pictures/icons/scss.png', category: ESkillCategory.DESIGN},
-  {name: 'sql', icon: 'src/assets/pictures/icons/sql.png', category: ESkillCategory.TECHNOLOGY},
-  {name: 'typescript', icon: 'src/assets/pictures/icons/typescript.png', category: ESkillCategory.TECHNOLOGY},
-  {name: 'vscode', icon: 'src/assets/pictures/icons/vscode.png', category: ESkillCategory.TOOL},
-  {name: 'vue', icon: 'src/assets/pictures/icons/vue.png', category: ESkillCategory.TECHNOLOGY}
+  {name: 'accessibility', icon: '/pictures/icons/accessibility.png', category: ESkillCategory.TOOL},
+  {name: 'csharp', icon: '/pictures/icons/csharp.png', category: ESkillCategory.TECHNOLOGY},
+  {name: 'css', icon: '/pictures/icons/css.png', category: ESkillCategory.DESIGN},
+  {name: 'figma', icon: '/pictures/icons/figma.png', category: ESkillCategory.DESIGN},
+  {name: 'git', icon: '/pictures/icons/git.png', category: ESkillCategory.TECHNOLOGY},
+  {name: 'html', icon: '/pictures/icons/html.png', category: ESkillCategory.TECHNOLOGY},
+  {name: 'javascript', icon: '/pictures/icons/javascript.png', category: ESkillCategory.TECHNOLOGY},
+  {name: 'jira', icon: '/pictures/icons/jira.png', category: ESkillCategory.TOOL},
+  {name: 'netcore', icon: '/pictures/icons/netcore.png', category: ESkillCategory.TECHNOLOGY},
+  {name: 'nuxt', icon: '/pictures/icons/nuxt.png', category: ESkillCategory.TECHNOLOGY},
+  {name: 'php', icon: '/pictures/icons/php.png', category: ESkillCategory.TECHNOLOGY},
+  {name: 'python', icon: '/pictures/icons/python.png', category: ESkillCategory.TECHNOLOGY},
+  {name: 'sass', icon: '/pictures/icons/scss.png', category: ESkillCategory.DESIGN},
+  {name: 'sql', icon: '/pictures/icons/sql.png', category: ESkillCategory.TECHNOLOGY},
+  {name: 'typescript', icon: '/pictures/icons/typescript.png', category: ESkillCategory.TECHNOLOGY},
+  {name: 'vscode', icon: '/pictures/icons/vscode.png', category: ESkillCategory.TOOL},
+  {name: 'vue', icon: '/pictures/icons/vue.png', category: ESkillCategory.TECHNOLOGY}
 ] 
